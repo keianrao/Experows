d837e96960c163bc0d3e00e0c41fe36f20437798
Keian Rao <keian.rao@gmail.com>
2020-10-21 18:52:41 -0400

    Added third row, 'scrollable click sink'.

--- experows.js
+++ experows.js
@@ -1,13 +1,13 @@
 /*
 * Experows - Demo of some JavaScript APIs
 * Written in 2020 by Keian Rao <keian.rao@gmail.com>
-* 
-* To the extent possible under law, the author has dedicated 
+*
+* To the extent possible under law, the author has dedicated
 * all copyright and related and neighboring rights to this software to
-* the public domain worldwide. 
-* 
+* the public domain worldwide.
+*
 * This software is distributed without any warranty.
-* 
+*
 * You should have received a copy of the CC0 Public Domain Dedication
 * along with this software. If not,
 * see <http://creativecommons.org/publicdomain/zero/1.0/>.
@@ -21,6 +21,7 @@ inpageConsole.println = (line) => inpageConsole.innerText += line + "\n";
 
 enableRow1().catch(error => inpageConsole.println(error));
 enableRow2().catch(error => inpageConsole.println(error));
+enableRow3().catch(error => inpageConsole.println(error));
 
 
 //  \\  //  \\  //  \\  //  \\  //  \\  //  \\  //  \\
@@ -51,10 +52,10 @@ async function enableRow2() {
 	/*
 	* Canonical image link:
 	* "https://commons.wikimedia.org/wiki/File:AD2009Sep20_Amanita_muscaria_02.jpg"
-	* 'Amanita muscaria', 'fly agaric', a poisonous mushroom. 
+	* 'Amanita muscaria', 'fly agaric', a poisonous mushroom.
 	* Picture by Bernie Kohl.
 	*/
-	const IMAGE_URL = 
+	const IMAGE_URL =
 		"https://upload.wikimedia.org/wikipedia/commons/" +
 		"thumb/8/8c/AD2009Sep20_Amanita_muscaria_02.jpg/" +
 		"320px-AD2009Sep20_Amanita_muscaria_02.jpg";
@@ -66,7 +67,7 @@ async function enableRow2() {
 	const imageReq = new XMLHttpRequest();
 
 	rowConsole.innerText = "Preparing XMLHttpRequest.\n";
-	
+
 	// Set request properties..
 	imageReq.timeout = 30000;
 	rowConsole.innerText += "Timeout set to " + imageReq.timeout + " milliseconds.\n";
@@ -99,7 +100,7 @@ async function enableRow2() {
 				canvas.style.setProperty("border-style", "solid");
 			})
 			.catch(function (error) {
-				rowConsole.innerText += 
+				rowConsole.innerText +=
 					"Failed to create drawable bitmap from image data.\n";
 				return;
 			});
@@ -122,3 +123,65 @@ async function enableRow2() {
 	rowConsole.innerText += " Done.\n\n";
 }
 
+
+async function enableRow3() {
+    const CANVAS_PANE_WIDTH = 160;
+    const CANVAS_PANE_HEIGHT = 120;
+    const CANVAS_WIDTH = 320;
+    const CANVAS_HEIGHT = 240;
+
+    const canvasPane = document.getElementById("3-output-pane");
+    const canvas = document.getElementById("3-output");
+    const canvasBuffer = document.createElement('canvas');
+    const rowConsole = document.getElementById("3-console");
+
+    rowConsole.innerText = "Setting up canvas..\n";
+    canvas.setAttribute("width", CANVAS_WIDTH);
+    canvas.setAttribute("height", CANVAS_HEIGHT);
+    canvas.style.setProperty("background", "#FFF4");
+    canvasPane.style.setProperty("width", CANVAS_PANE_WIDTH + "px");
+    canvasPane.style.setProperty("height", CANVAS_PANE_HEIGHT + "px");
+    canvasPane.style.setProperty("border-style", "solid");
+    canvasPane.style.setProperty("overflow-x", "scroll");
+    canvasPane.style.setProperty("overflow-y", "scroll");
+    canvasPane.style.setProperty("cursor", "crosshair");
+    canvasBuffer.setAttribute("width", CANVAS_WIDTH);
+    canvasBuffer.setAttribute("height", CANVAS_HEIGHT);
+    // Any particular reason why we're setting all of these here
+    // intead of in our page stylesheet?
+
+    rowConsole.innerText += "Adding controls and drawing mechanisms.\n";
+    canvas.addEventListener('mousemove', function (mouseEvent) {
+        const canvasContext = canvas.getContext('2d');
+        // Are these saved into the event listener?
+        // Or are they re-evaluated every time?
+
+        canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Okay, redraw cursor lines.
+        canvasContext.fillRect(0, mouseEvent.offsetY, canvas.width, 1);
+        canvasContext.fillRect(mouseEvent.offsetX, 0, 1, canvas.height);
+
+        // Then redraw buffer.
+        canvasContext.drawImage(canvasBuffer, 0, 0);
+    });
+    canvas.addEventListener('click', function (mouseEvent) {
+        const canvasBufferContext = canvasBuffer.getContext('2d');
+
+        rowConsole.innerText +=
+            " " + mouseEvent.offsetX + "," + mouseEvent.offsetY + ".";
+        // Apparently, innerText#+ ignores trailing spaces.
+
+        // Draw a dot on the buffer.
+        canvasBufferContext.beginPath();
+        canvasBufferContext.ellipse(
+            mouseEvent.offsetX, mouseEvent.offsetY,
+            4, 4,
+            0, 0, 2 * Math.PI
+        );
+        canvasBufferContext.closePath();
+        canvasBufferContext.fill();
+    });
+
+    rowConsole.innerText += "Ready. Click in the canvas!\n";
+}
