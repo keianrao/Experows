43d3f044143d6f81cdb5b3a81f3aeaa795f52f8f
Keian Rao <keian.rao@gmail.com>
2020-10-21 18:56:52 -0400

    Slight fixes to earlier commit

--- experows.js
+++ experows.js
@@ -125,63 +125,63 @@ async function enableRow2() {
 
 
 async function enableRow3() {
-    const CANVAS_PANE_WIDTH = 160;
-    const CANVAS_PANE_HEIGHT = 120;
-    const CANVAS_WIDTH = 320;
-    const CANVAS_HEIGHT = 240;
+	const CANVAS_PANE_WIDTH = 160;
+	const CANVAS_PANE_HEIGHT = 120;
+	const CANVAS_WIDTH = 320;
+	const CANVAS_HEIGHT = 240;
 
-    const canvasPane = document.getElementById("3-output-pane");
-    const canvas = document.getElementById("3-output");
-    const canvasBuffer = document.createElement('canvas');
-    const rowConsole = document.getElementById("3-console");
+	const canvasPane = document.getElementById("3-output-pane");
+	const canvas = document.getElementById("3-output");
+	const canvasBuffer = document.createElement('canvas');
+	const rowConsole = document.getElementById("3-console");
 
-    rowConsole.innerText = "Setting up canvas..\n";
-    canvas.setAttribute("width", CANVAS_WIDTH);
-    canvas.setAttribute("height", CANVAS_HEIGHT);
-    canvas.style.setProperty("background", "#FFF4");
-    canvasPane.style.setProperty("width", CANVAS_PANE_WIDTH + "px");
-    canvasPane.style.setProperty("height", CANVAS_PANE_HEIGHT + "px");
-    canvasPane.style.setProperty("border-style", "solid");
-    canvasPane.style.setProperty("overflow-x", "scroll");
-    canvasPane.style.setProperty("overflow-y", "scroll");
-    canvasPane.style.setProperty("cursor", "crosshair");
-    canvasBuffer.setAttribute("width", CANVAS_WIDTH);
-    canvasBuffer.setAttribute("height", CANVAS_HEIGHT);
-    // Any particular reason why we're setting all of these here
-    // intead of in our page stylesheet?
+	rowConsole.innerText = "Setting up canvas..\n";
+	canvas.setAttribute("width", CANVAS_WIDTH);
+	canvas.setAttribute("height", CANVAS_HEIGHT);
+	canvas.style.setProperty("background", "#FFF4");
+	canvas.style.setProperty("cursor", "crosshair");
+	canvasPane.style.setProperty("width", CANVAS_PANE_WIDTH + "px");
+	canvasPane.style.setProperty("height", CANVAS_PANE_HEIGHT + "px");
+	canvasPane.style.setProperty("border-style", "solid");
+	canvasPane.style.setProperty("overflow-x", "scroll");
+	canvasPane.style.setProperty("overflow-y", "scroll");
+	canvasBuffer.setAttribute("width", CANVAS_WIDTH);
+	canvasBuffer.setAttribute("height", CANVAS_HEIGHT);
+	// Any particular reason why we're setting all of these here
+	// intead of in our page stylesheet?
 
-    rowConsole.innerText += "Adding controls and drawing mechanisms.\n";
-    canvas.addEventListener('mousemove', function (mouseEvent) {
-        const canvasContext = canvas.getContext('2d');
-        // Are these saved into the event listener?
-        // Or are they re-evaluated every time?
+	rowConsole.innerText += "Adding controls and drawing mechanisms.\n";
+	canvas.addEventListener('mousemove', function (mouseEvent) {
+		const canvasContext = canvas.getContext('2d');
+		// Are these saved into the event listener?
+		// Or are they re-evaluated every time?
 
-        canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+		canvasContext.clearRect(0, 0, canvas.width, canvas.height);
 
-        // Okay, redraw cursor lines.
-        canvasContext.fillRect(0, mouseEvent.offsetY, canvas.width, 1);
-        canvasContext.fillRect(mouseEvent.offsetX, 0, 1, canvas.height);
+		// Okay, redraw cursor lines.
+		canvasContext.fillRect(0, mouseEvent.offsetY, canvas.width, 1);
+		canvasContext.fillRect(mouseEvent.offsetX, 0, 1, canvas.height);
 
-        // Then redraw buffer.
-        canvasContext.drawImage(canvasBuffer, 0, 0);
-    });
-    canvas.addEventListener('click', function (mouseEvent) {
-        const canvasBufferContext = canvasBuffer.getContext('2d');
+		// Then redraw buffer.
+		canvasContext.drawImage(canvasBuffer, 0, 0);
+	});
+	canvas.addEventListener('click', function (mouseEvent) {
+		const canvasBufferContext = canvasBuffer.getContext('2d');
 
-        rowConsole.innerText +=
-            " " + mouseEvent.offsetX + "," + mouseEvent.offsetY + ".";
-        // Apparently, innerText#+ ignores trailing spaces.
+		rowConsole.innerText +=
+			" " + mouseEvent.offsetX + "," + mouseEvent.offsetY + ".";
+		// Apparently, innerText#+ ignores trailing spaces.
 
-        // Draw a dot on the buffer.
-        canvasBufferContext.beginPath();
-        canvasBufferContext.ellipse(
-            mouseEvent.offsetX, mouseEvent.offsetY,
-            4, 4,
-            0, 0, 2 * Math.PI
-        );
-        canvasBufferContext.closePath();
-        canvasBufferContext.fill();
-    });
+		// Draw a dot on the buffer.
+		canvasBufferContext.beginPath();
+		canvasBufferContext.ellipse(
+			mouseEvent.offsetX, mouseEvent.offsetY,
+			4, 4,
+			0, 0, 2 * Math.PI
+		);
+		canvasBufferContext.closePath();
+		canvasBufferContext.fill();
+	});
 
-    rowConsole.innerText += "Ready. Click in the canvas!\n";
+	rowConsole.innerText += "Ready. Click in the canvas!\n";
 }
